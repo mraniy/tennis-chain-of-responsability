f94a81871dbae4a227746ff36b89f27337b97a47
younes <mraniy@gmail.com>
2019-11-10 00:26:02 +0100

    remove useless added set when match ends

--- src/main/java/com/kata/tennis/service/MatchHandler.java
+++ src/main/java/com/kata/tennis/service/MatchHandler.java
@@ -4,10 +4,11 @@ import com.kata.tennis.model.Match;
 import com.kata.tennis.model.Player;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MatchHandler extends UnitScoreHandler {
 
-    private final int NUMBER_OF_SETS_TO_WIN_THE_TIE = 2;
+    private final int NUMBER_OF_SETS_TO_WIN_THE_TIE = 3;
 
     public MatchHandler() {
         next = Optional.empty();
@@ -17,7 +18,14 @@ public class MatchHandler extends UnitScoreHandler {
     public void refreshScore(Match match, Player player) {
         if(hasPlayer2WonTheMatch(match, player) || hasPlayer1WonTheMatch(match, player)) {
             setWinner(player,match);
+            removeLastSetAddedAndDecrementSetNumber(match);
         }
+    }
+
+    public void removeLastSetAddedAndDecrementSetNumber(Match match) {
+        match.getScore().getScorePlayer1().getNumberGamesWonByPlayerBySet().removeLast();
+        match.getScore().getScorePlayer2().getNumberGamesWonByPlayerBySet().removeLast();
+        match.setSetNumber(new AtomicInteger(match.getSetNumber()).decrementAndGet());
     }
 
     private boolean hasPlayer1WonTheMatch(Match match, Player player) {
@@ -30,6 +38,7 @@ public class MatchHandler extends UnitScoreHandler {
 
 
     private void setWinner(Player player, Match match1) {
+
         match1.setWinner(Optional.of(player.getName()));
     }
 
